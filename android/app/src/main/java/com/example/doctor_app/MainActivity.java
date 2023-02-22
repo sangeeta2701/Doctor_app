d44597aabd0c8357a8e781b6d3df060779a08981
DELL <anjugupta7060@gmail.com>
2023-02-22 20:07:42 +0530

    Appointment Booking App

---
+++ android/app/src/main/java/com/example/doctor_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.doctor_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color appUiLightColor = Colors.white;
+const Color appUiDarkColor = Colors.black;
+const Color appUiThemeColor = Color.fromRGBO(0, 139, 139, 1);
