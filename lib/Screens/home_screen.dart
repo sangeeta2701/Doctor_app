7a4ec146fa4c186024d08dd2495ecf25465960cc
DELL <anjugupta7060@gmail.com>
2023-02-23 20:07:35 +0530

    Appointment Booking App

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -66,16 +66,75 @@ class HomeScreen extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             CircleAvatar(
                               radius: 30,
+                              backgroundImage:
+                                  AssetImage("assets/images/img4.png"),
                             ),
+                            Icon(
+                              Icons.notifications_none,
+                              color: appUiLightColor,
+                              size: 30,
+                            )
                           ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 15.0),
+                          child: Text(
+                            "Hi,Dear",
+                            style: TextStyle(
+                              color: appUiLightColor,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10.0),
+                          child: Text(
+                            "Your Health is Our\nFirst Priority",
+                            style: TextStyle(
+                              color: appUiLightColor,
+                              fontSize: 25,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 15, bottom: 20),
+                          width: MediaQuery.of(context).size.width,
+                          height: 55,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                              color: appUiLightColor,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: appUiLightBlackColor,
+                                  blurRadius: 6,
+                                  spreadRadius: 3,
+                                ),
+                              ]),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Search Here....",
+                              hintStyle: TextStyle(
+                                color: appUiDarkColor.withOpacity(0.5),
+                              ),
+                              prefixIcon: Icon(
+                                Icons.search,
+                                size: 25,
+                              ),
+                            ),
+                          ),
                         ),
                       ],
                     ),
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const Color appUiLightColor = Colors.white;
 const Color appUiDarkColor = Colors.black;
 const Color appUiThemeColor = Color.fromRGBO(0, 139, 139, 1);
+const Color appUiLightBlackColor = Colors.black12;
